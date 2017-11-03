e5046e679b7a43aaae65f89f2b33f1cb7a6ddf28
stormxing <stormxing@foxmail.com>
2017-11-03 08:43:26 +0000

    section2

--- 任务卡106 - JavaScript 编程基础 - Pre-Pos（集合运算）/main/section-2/practice-1.js
+++ 任务卡106 - JavaScript 编程基础 - Pre-Pos（集合运算）/main/section-2/practice-1.js
@@ -1,23 +1,19 @@
-'use strict';
+ 'use strict';
 
 module.exports = function countSameElements(collection) {
-    let key = [];
-    let count = 0;
-    let result = [];
-
-    for(let i=0;i<collection.length;i++){
-        if(collection[i]!== -1){
-            key=collection[i];
-            for(let j=0;j<collection.length;j++){
-                if(key === collection[j]){
-                    count++;
-                    collection[j] = -1;
-                }
-            }
-            result.push('key'+key[i] + ":" + 'count'+count);
-            count = 0;
+    let array = [];
+    let count = 1;
+    for(let i = 0;i<collection.length;i++){
+        if(collection[i] == collection[i+1]){
+            count++;
+        }else{
+            array.push({
+                key: collection[i-1],
+                count: count
+            });
+            count = 1;
         }
     }
-
-    return result;
+    
+    return array;
 }
--- 任务卡106 - JavaScript 编程基础 - Pre-Pos（集合运算）/main/section-2/practice-2.js
+++ 任务卡106 - JavaScript 编程基础 - Pre-Pos（集合运算）/main/section-2/practice-2.js
@@ -1,5 +1,26 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  let array = [];
+    let count = 1;
+    for(let i = 0;i<collection.length;i++){
+        if(collection[i] == collection[i+1]){
+            count++;
+        }else{
+            if(collection[i].split("-").length===1){
+                array.push({
+                    key: collection[i-1],
+                    count: count
+                });
+                count = 1;
+            }else{
+                array.push({
+                    key: collection[i].split("-")[0],
+                    count: parseInt(collection[i].split("-")[1])
+                });
+            }
+        }
+    }
+    
+    return array;
 }
